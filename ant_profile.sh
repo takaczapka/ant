94a1e9abff9d1dea2e4f4ee5e47f4652316e1a60
Maciej Falski <maciej.falski@gmail.com>
2017-11-12 12:31:44 +0000

    more ant aliases added for the full build cycle

--- ant_profile.sh
+++ ant_profile.sh
@@ -2,7 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+GRADLEW_OPTS="--console plain"
 
 alias ant="cd $DIR"
-alias antb="ant; ./gradlew clean build"
-alias antui="ant; ./gradlew -t uiBuild"
+alias antb="ant && ./gradlew ${GRADLEW_OPTS} clean build test"
+alias ant-stest="ant; ./gradlew ${GRADLEW_OPTS} scalaTest"
+alias antui="ant && ./gradlew ${GRADLEW_OPTS} -t uiBuild"
+alias antjar="ant && ./gradlew ${GRADLEW_OPTS} jar"
+alias antui-test="ant && ./gradlew ${GRADLEW_OPTS} uiTest"
+alias antui-refresh="ant && ./gradlew ${GRADLEW_OPTS} uiNuke uiInstall uiBuild"
+alias antui-install="ant && ./gradlew ${GRADLEW_OPTS} uiInstall"
+alias antui-nuke="ant && ./gradlew ${GRADLEW_OPTS} uiNuke"
+alias ffsp='ant && figlet "Git rebase FFS..." && git pull --rebase && figlet "Building FFS..." && antb && figlet "Pushing FFS..." && git push'
