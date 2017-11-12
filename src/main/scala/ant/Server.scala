609f8dafea30370ec0305ad205968f1f10b9ae6d
Maciej Falski <maciej.falski@gmail.com>
2017-11-12 12:30:18 +0000

    static files service added

--- src/main/scala/ant/Server.scala
+++ src/main/scala/ant/Server.scala
@@ -1,13 +1,28 @@
 package ant
 
+import fs2.interop.cats._
 import fs2.{Stream, Task}
+import org.http4s.dsl.{->, GET, NotFound, Root, _}
 import org.http4s.server.blaze.BlazeBuilder
+import org.http4s.server.staticcontent
+import org.http4s.server.staticcontent.ResourceService
 import org.http4s.util.StreamApp
+import org.http4s.{HttpService, StaticFile}
 
 object Server extends StreamApp {
   override def stream(args: List[String]): Stream[Task, Nothing] =
     BlazeBuilder
       .bindHttp(8080, "0.0.0.0")
+      .mountService(root)
+      .mountService(staticFileService)
       .mountService(HelloWorld.service)
       .serve
+
+  val staticFileService: HttpService = staticcontent.resourceService(ResourceService.Config(basePath = "/webapp", pathPrefix = "/resources"))
+
+  val root = HttpService {
+    case request@GET -> Root =>
+      StaticFile.fromResource("/webapp/index.html", Some(request)).getOrElseF(NotFound())
+  }
+
 }
